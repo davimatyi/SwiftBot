86c4e7720e86087c7229c27830351bcac1e83e53
Marsu5 <marcell.schuh@gmail.com>
2017-02-01 08:52:16 +0100

    Added config.json

--- bot.js
+++ bot.js
@@ -1,28 +1,31 @@
-var Discord = require("discord.js");
+const Discord = require("discord.js");
 const api = require("./utils/API.js");
-var client = new Discord.Client();
+const client = new Discord.Client();
 const cat = require("./handlers/cat.js");
 const chuck = require("./handlers/chuck.js");
+const config = require("./config.json");
 
 
 client.on("message", msg => {
-    let prefix = "!";
-    if(!msg.content.startsWith(prefix)&& msg.toString().length > 1) return;
-    let prefixLess = msg.content.substring(prefix.length);
+    if(!msg.content.startsWith(config.prefix) && msg.toString().length > 1) return;
+    if(msg.author.bot) return;
+    // Removes prefix
+    let prefixLess = msg.content.substring(config.prefix.length);
 
-    //Func commands
+    // Fun commands
     if(prefixLess === "cat") cat.sendRandomCat(msg);
     if(prefixLess.startsWith("ping")) msg.channel.sendMessage("pong!",{tts: true});
     if(prefixLess.startsWith("chuck")) chuck.sendRandomChuck(msg);
     if(prefixLess.startsWith("suck")) msg.channel.sendMessage(prefixLess.substring(5)+" sucks!",{tts: true});
 });
 
-//Commands with different prefixes
+// Commands with different prefixes
 client.on("message", msg => {
+    if(msg.author.bot) return;
     if(msg.content.startsWith("$cat")){
         for (var i = 0; i < 8; i++) {
             cat.sendRandomCat(msg);
-       }
+        }
     }
 });
 
