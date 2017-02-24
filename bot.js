5e98f2825ccf0870695512a73c980142282a31f7
Marsu5 <marcell.schuh@gmail.com>
2017-02-24 18:52:00 +0100

    Join voice channel

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 const cat = require("./handlers/cat.js");
 const chuck = require("./handlers/chuck.js");
 const config = require("./config.json");
+const utils = require("./utils/Utils.js");
 
 
 client.on("message", msg => {
@@ -11,12 +12,17 @@ client.on("message", msg => {
     if(msg.author.bot) return;
     // Removes prefix
     let prefixLess = msg.content.substring(config.prefix.length);
-
     // Fun commands
     if(prefixLess === "cat") cat.sendRandomCat(msg);
     if(prefixLess.startsWith("ping")) msg.channel.sendMessage("pong!",{tts: true});
     if(prefixLess.startsWith("chuck")) chuck.sendRandomChuck(msg);
     if(prefixLess.startsWith("suck")) msg.channel.sendMessage(prefixLess.substring(5)+" sucks!",{tts: true});
+    if(msg.author.id === "174786282793205760"){
+        if(prefixLess === "dChannels"){
+            utils.findAuthorVoiceChannel(msg).join();
+        }
+        
+    }
 });
 
 // Commands with different prefixes
@@ -32,7 +38,8 @@ client.on("message", msg => {
 client.on("ready", () => {
     console.log("I have succsessfuly logged in as: " + client.user.username);
     client.user.setGame("Shake It OFF");
-    client.user.setAFK(true);
 });
 
 client.login(api.token);
+
+
