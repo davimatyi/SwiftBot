09ddc76342aaadea01d80382a1d9617ce268d059
mrclsu <marcell.schuh@gmail.com>
2018-12-11 10:16:06 +0100

    Misc fixes

--- handlers/swift.js
+++ handlers/swift.js
@@ -7,6 +7,7 @@ function play(msg){
         msg.channel.sendMessage("Please join a voice channel before running this command!");
         return;
     }
+
     utils.findAuthorVoiceChannel(msg).join()
         .then(connection => {
             const dispatcher = connection.playFile("./audio/shakeitoff.mp3");
@@ -14,7 +15,7 @@ function play(msg){
                 console.log(err);
             });
             dispatcher.on("end", reason => {
-                //console.log(reason);
+                //console.log(reaso1n);
                 msg.member.voiceChannel.leave();
             });
             //console.log(true);
